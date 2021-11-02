3c3acc49fc8ffea8bb65bc748cf0d72ebaf6b82d
Taewoong1378 <ktw2378@korea.ac.kr>
2021-11-02 10:48:08 +0900

    인터페이스 공부 완료

---
+++ practice-typescript/class-note/3_interface.ts
@@ -0,0 +1,69 @@
+interface User {
+  age: number;
+  name: string;
+}
+
+// 변수에 활용한 인터페이스
+const taewoong: User = {
+  age: 22,
+  name: '태웅'
+}
+
+// 함수에 인터페이스 활용
+function getUser(user: User): void {
+  console.log(user);
+}
+
+const capt = {
+  name: '캡틴',
+  age: 33
+}
+getUser(capt);
+
+// 함수의 스펙(구조)에 인터페이스를 활용
+interface SumFunction {
+  (a: number, b: number): number;
+}
+
+let sum: SumFunction;
+sum = function(a: number, b: number): number {
+  return a + b;
+}
+
+// 인덱싱
+interface StringArray {
+  [index: number]: string;
+}
+
+const arr2: StringArray = ['a', 'b', 'c'];
+// arr2[0] = 'd';
+
+// 딕셔너리 패턴
+interface StringRegexDictionary {
+  [key: string]: RegExp;
+}
+
+const obj2: StringRegexDictionary = {
+  // sth: /abc/,
+  cssFile: /\.css$/,
+  jsFile: /\.js$/,
+}
+obj2[2] = /\.css$/;
+
+
+// 인터페이스 확장 (상속)
+interface Person {
+  name: string;
+  age: number;
+}
+
+interface Developer extends Person {
+  language: string;
+}
+
+// language만 정의하면 에러가 난다.
+let captain: Developer = {
+  name: 'taewoong',
+  age: 22,
+  language: 'Korean',
+};
