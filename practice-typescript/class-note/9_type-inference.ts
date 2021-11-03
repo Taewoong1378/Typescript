bccdc6b62dc66b3ffee420bd6759850482c35237
Taewoong1378 <ktw2378@korea.ac.kr>
2021-11-03 09:12:58 +0900

    타입 추론 학습완료

--- practice-typescript/class-note/9_type-inference.ts
+++ practice-typescript/class-note/9_type-inference.ts
@@ -1,3 +1,4 @@
+// 타입 추론 기본 1
 let a = 'abc';
 
 function getB(b = 10) {
@@ -6,3 +7,33 @@ function getB(b = 10) {
 }
 
 // 10 + '10' -> 1010
+
+// 타입 추론 기본 2
+// interface Dropdown<T> {
+//   value: T;
+//   title: string;
+// }
+// let shoppingItem: Dropdown<string> = {
+//   value: 'abc',
+//   title: 'hello'
+// }
+
+// 타입 추론 기본 3
+interface Dropdown<T> {
+  value: T;
+  title: string;
+}
+interface DetailedDropdown<K> extends Dropdown<K> {
+  description: string;
+  tag: K;
+}
+
+let detailedItem: DetailedDropdown<string> = {
+  title: 'abc',
+  description: 'ab',
+  value: 'abc',
+  tag: 'hello',
+};
+
+// Best Common Type
+let arr = [1, 2, true, 'a'];
